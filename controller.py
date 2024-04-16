9046b0064809f1904678694cfcb1cfd3d51281b0
vassi <asrtjlbv@ffgh.com>
2024-04-16 18:38:06 +0300

    bruh

--- controller.py
+++ controller.py
@@ -1,23 +1,35 @@
 import pygame, model,random,bullet
 
+p = pygame.event.custom_type()
+
+you_can=True
 def comtrol():
+    global you_can
 
     event=pygame.event.get()
     model.p.control(event)
     model.k.controller(event)
+
     for a in model.q:
         a.control(event)
+    print(pygame.mouse.get_pressed())
+    if pygame.mouse.get_pressed()[0] and you_can:
+        model.h.append(bullet.Bullet(model.q, model.p, model.h))
+        you_can = False
+        pygame.time.set_timer(p, 300, 1)
+    for e in event.copy():
+        if e.type == p:
+            you_can=True
 
-    for e in event:
         if e.type==pygame.QUIT:
             exit()
         if e.type==pygame.KEYDOWN and e.key==pygame.K_TAB:
             model.event_for_debug=not model.event_for_debug
         if e.type==pygame.KEYDOWN and e.key==pygame.K_q:
             model.normal_paint=not model.normal_paint
-        if e.type==pygame.MOUSEBUTTONDOWN:
 
-            model.h.append(bullet.Bullet(model.q, model.p,model.h))
+
+
 
 
         if e.type == pygame.KEYDOWN and e.key == pygame.K_a:
